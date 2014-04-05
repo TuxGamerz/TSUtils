f72ea65c5235ead7afe0a13f5152f6f6b4f61e7f
TuxGamerz <tuxgamerz@live.com>
2014-04-05 23:12:05 +0100

    Modified mongoclient class batter in

--- src/com/stronghearttarot/tsutils/utils/MongoConnector.java
+++ src/com/stronghearttarot/tsutils/utils/MongoConnector.java
@@ -1,22 +1,36 @@
 package com.stronghearttarot.tsutils.utils;
 
 import java.net.UnknownHostException;
+import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 public class MongoConnector
 {
 	private MongoClient mongoClient;
+	private DB serverDatabase;
+	private Logger log = Bukkit.getLogger();
 	
 	public MongoConnector()
 	{
 		try
 		{
-			mongoClient = new MongoClient();
+			mongoClient = new MongoClient("localhost", 27017);
+			serverDatabase = mongoClient.getDB("mc_server");
 		} catch (UnknownHostException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	/	{
+			log.info("Error: " + e.getLocalizedMessage());
 		}
 	}
+	
+	public void playerHomes(String playerUuid, double loc_x, double loc_y, double loc_z)
+	{
+		DBCollection playerHomes = serverDatabase.getCollection("player_homes");
+		BasicDBObject playerHomeInfo =  
+	}
+	
 }

4b30da2d95b987d1b3fa9c78ef45f4810c9e916c
skybinary <skybinary.lucid@gmail.com>
2014-04-06 01:56:41 +0100

    Instantiated main class

--- src/main/java/com/stronghearttarot/tsutils/utils/MongoConnector.java
+++ src/main/java/com/stronghearttarot/tsutils/utils/MongoConnector.java
@@ -4,6 +4,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.stronghearttarot.tsutils.TSUtils;
 import org.bukkit.Bukkit;
 
 import java.net.UnknownHostException;
@@ -16,6 +17,7 @@ public class MongoConnector {
     private MongoClient mongoClient;
     private DB serverDatabase;
     private Logger log = Bukkit.getLogger();
+    private TSUtils plugin;
 
     public MongoConnector() {
         try {
